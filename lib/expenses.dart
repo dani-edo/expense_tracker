3595fb8d5d7e6ded34e3dca11c350506a66f5215
dani_edo <dani.edogawa.17@gmail.com>
2023-12-24 13:35:32 +0700

    add enum category

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
-  const Expenses({Key? key}) : super(key: key);
+  const Expenses({super.key});
 
   @override
   _ExpensesState createState() => _ExpensesState();
@@ -10,6 +10,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list'),
+        ],
+      ),
+    );
   }
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.amount,
     required this.title,
     required this.date,
+    required this.category,
   }) : id = uuid.v4(); // generate unique id whenever this class initialized
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
